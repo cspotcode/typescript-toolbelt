a8b16de5728d9820609417a8425ccc78958fdada
Andrew Bradley <cspotcode@gmail.com>
2018-05-03 13:33:25 -0400

    Update experiments.ts

--- experiments.ts
+++ experiments.ts
@@ -77,3 +77,19 @@ function buildObject<T extends object>(value: T = {}): ObjectBuilder<T> {
     builder.value = value;
     return builder as any;
 }
+
+
+/**
+ * Map a type, replacing the `[x: string]: any` with `[x: string]: U`
+ * JS typechecking automatically adds a string index signature to object literals.
+ * I haven't devised a way to strip the string index signature, but we can at least
+ * customize it.
+ */
+export type ReplaceStringIndex<T, U> = {
+    [K in keyof T]:
+    K extends string ? string extends K
+        // K is string and not a string literal; change its value
+        ? U
+        // otherwise leave this property untouched
+        : K : K;
+}
