7659cdee1e0be9a2954ca34a22a61f93b4e7495d
Andrew Bradley <cspotcode@gmail.com>
2018-05-03 13:19:15 -0400

    Update experiments.ts

--- experiments.ts
+++ experiments.ts
@@ -49,3 +49,31 @@ export type DeepReadonlyObject<T> = {
 
 export type A = DeepMutable<ReadonlyArray<'hello'>>;
 const a = null as any as A;
+
+
+/** Optional chaining operator semantics */
+function OCO(value, prop1, prop2, prop3, prop4) { 
+    if(value == null) return undefined; 
+    const value1 = value[prop1]; 
+    if(value1 == null || prop2 == null) return undefined; 
+    const value2 = value1[prop2]; 
+    if(value2 == null || prop3 == null) return undefined; 
+    const value3 = value2[prop3]; 
+    if(value3 == null || prop4 == null) return undefined; 
+    const value4 = value3[prop4]; 
+    return value4; 
+}
+
+export interface ObjectBuilder<T> {
+    value: {[K in keyof T]: T[K]};
+    <K extends string, V>(key: K, value: V): ObjectBuilder<ReplaceProps<T, {[Key in K]: V}>>;
+}
+
+function buildObject<T extends object>(value: T = {}): ObjectBuilder<T> {
+    function builder(key, value) {
+        value[key] = value;
+        return builder;
+    }
+    builder.value = value;
+    return builder as any;
+}
