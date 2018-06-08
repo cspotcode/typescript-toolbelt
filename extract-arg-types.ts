6b65ca8a15c8008733ff6aec3a2c087c6228d408
Andrew Bradley <cspotcode@gmail.com>
2018-06-08 16:18:19 -0400

    Create extract-arg-types.ts

---
+++ extract-arg-types.ts
@@ -0,0 +1,17 @@
+// These two are already built-in:
+// ReturnType: extracts the return type of a function.
+// InstanceType: extracts the instance type of a class.
+
+// ... but what about extracting the arguments of a function?
+type Argument1Type<Fn extends (arg1: any, ...rest: any[]) => any> = Fn extends (arg1: infer A, ...rest: any[]) => any ? A : any;
+type Argument2Type<Fn extends (arg1: any, arg2: any, ...rest: any[]) => any> = Fn extends (arg1: any, arg2: infer A, ...rest: any[]) => any ? A : any;
+type Argument3Type<Fn extends (arg1: any, arg2: any, arg3: any, ...rest: any[]) => any> = Fn extends (arg1: any, arg2: any, arg3: infer A, ...rest: any[]) => any ? A : any;
+type Argument4Type<Fn extends (arg1: any, arg2: any, arg3: any, arg4: any, ...rest: any[]) => any> = Fn extends (arg1: any, arg2: any, arg3: any, arg4: infer A, ...rest: any[]) => any ? A : any;
+// ...and so on
+
+// ... or extracting the arguments to a constructor
+type ConstructorArgument1Type<Fn extends new (arg1: any, ...rest: any[]) => any> = Fn extends new (arg1: infer A, ...rest: any[]) => any ? A : any;
+type ConstructorArgument2Type<Fn extends new (arg1: any, arg2: any, ...rest: any[]) => any> = Fn extends new (arg1: any, arg2: infer A, ...rest: any[]) => any ? A : any;
+type ConstructorArgument3Type<Fn extends new (arg1: any, arg2: any, arg3: any, ...rest: any[]) => any> = Fn extends new (arg1: any, arg2: any, arg3: infer A, ...rest: any[]) => any ? A : any;
+type ConstructorArgument4Type<Fn extends new (arg1: any, arg2: any, arg3: any, arg4: any, ...rest: any[]) => any> = Fn extends new (arg1: any, arg2: any, arg3: any, arg4: infer A, ...rest: any[]) => any ? A : any;
+// ...and so on
