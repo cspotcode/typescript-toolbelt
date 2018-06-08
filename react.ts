f6aaa79ac2b81864c746a18cbfac741c0c2651fc
Andrew Bradley <cspotcode@gmail.com>
2018-06-08 17:36:59 -0400

    Create react.ts

---
+++ react.ts
@@ -0,0 +1,19 @@
+import {ComponentClass, Component} from 'react';
+
+/** Extract Props type of a Component (instance) type */
+type PropsType<C extends Component<any>> = C extends Component<infer P> ? P : never;
+
+/** Extract Props type of a Component factory type (a function that returns a component instance) */
+type PropsTypeOfFactory<CF extends (...args: any[]) => Component> = PropsType<ReturnType<CF>>;
+
+/** Given a ComponentClass type (constructor function), extracts Props type */
+type PropsTypeOfClass<CC extends ComponentClass> = PropsType<InstanceType<CC>>;
+
+/** Extract State type of a Component (instance) type */
+type StateType<C extends Component> = C extends Component<any, infer S> ? S : never;
+
+/** Extract State type of a Component factory type (a function that returns a component instance) */
+type StateTypeOfFactory<CF extends (...args: any[]) => Component> = StateType<ReturnType<CF>>;
+
+/** Extract State type of a ComponentClass type (constructor function) */
+type StateTypeOfClass<CC extends ComponentClass> = StateType<InstanceType<CC>>;
